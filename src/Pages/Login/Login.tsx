16abcdd8a40114dd19c22d9155914ee7bd14d705
Alexiskeepcoding <alexissotomayor95@gmail.com>
2023-10-21 20:30:44 -0500

    last fix :'c

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form';
 import {get, isEmpty } from 'lodash';
 import { IUserApp, useUserCredentials } from './UserCredentials/UserCredentials';
@@ -39,12 +38,6 @@ function Login() {
 
         }
     }
-
-    useEffect(() => {
-        console.log("object is: ", auth);
-    }, [])
-
-
 
 
   return (
